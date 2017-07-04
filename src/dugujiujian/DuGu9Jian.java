c230bb4447acdffbf2f10cc530ff81bb88ea4175
zhangwuji123456 <726181747@qq.com>
2017-07-04 23:48:40 +0800

    wuji

--- src/dugujiujian/DuGu9Jian.java
+++ src/dugujiujian/DuGu9Jian.java
@@ -4,7 +4,9 @@ public class DuGu9Jian {
    
 
 	public static void main(String[] args) {
-		 System.out.println("独孤九剑");
+		 System.out.println("独孤九剑1");
+		 System.out.println("独孤九剑2");
+		 System.out.println("独孤九剑3");
 
 	}
 
